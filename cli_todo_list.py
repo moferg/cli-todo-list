8afeabdf61a5e03eebf34ae385a91d5382f592cf
Marshall Ferguson <moferg01@louisville.edu>
2020-07-04 08:40:11 -0400

    added TODOs

---
+++ cli_todo_list.py
@@ -0,0 +1,9 @@
+# CLI TODO LIST - Marshall Ferguson - 7/2020
+
+# TODO - Prompt the user to input some TODOs
+# TODO - Output the TODOs to the user
+# TODO - Ask the user if they want to add more TODOs, check some off, or exit the app
+    # TODO - Give user ability to add more TODOs
+    # TODO - Give user ability to check off TODOs
+    # TODO - Give user ability to exit app from menu
+# TODO - Give user ability to sort TODOs by date, alphabetically, etc.
