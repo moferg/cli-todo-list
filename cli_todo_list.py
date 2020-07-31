fbe85203ffcad439333e482da38782b5c3a6505f
Marshall Ferguson <moferg01@louisville.edu>
2020-07-31 08:30:30 -0400

    added code to add TODOs to a list and print out list

--- cli_todo_list.py
+++ cli_todo_list.py
@@ -1,7 +1,17 @@
 # CLI TODO LIST - Marshall Ferguson - 7/2020
 
+print("Welcome to the TODO List App!")
+
 # TODO - Prompt the user to input some TODOs
+
+todo_list = []
+todo = input("Please enter a TODO to add to your list.     ")
+todo_list.append(todo)
+
 # TODO - Output the TODOs to the user
+
+print(todo_list)
+
 # TODO - Ask the user if they want to add more TODOs, check some off, or exit the app
     # TODO - Give user ability to add more TODOs
     # TODO - Give user ability to check off TODOs
